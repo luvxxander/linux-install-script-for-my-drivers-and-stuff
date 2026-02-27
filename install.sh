f3ba78a43c56fcdc55f4cb53f4a5d23893788d4a
luvxxander <86071709+luvxxander@users.noreply.github.com>
2026-02-26 19:04:01 -0800

    initial arch install script

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+sudo pacman -Syu --noconfirm
+
+sudo sed -i '/\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
+sudo pacman -Sy --noconfirm
+
+sudo pacman -S --noconfirm \
+base-devel \
+git \
+wget \
+curl \
+linux-headers \
+networkmanager \
+pipewire \
+pipewire-pulse \
+wireplumber \
+bluez \
+bluez-utils \
+mesa \
+vulkan-radeon \
+lib32-mesa \
+lib32-vulkan-radeon \
+amd-ucode \
+linux-firmware \
+asusctl \
+supergfxctl \
+tlp \
+acpi \
+acpi_call
+
+sudo systemctl enable NetworkManager
+sudo systemctl enable bluetooth
+sudo systemctl enable tlp
+sudo systemctl enable supergfxd
+
+cd /tmp
+rm -rf yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+
+yay -S --noconfirm \
+steam \
+discord \
+visual-studio-code-bin \
+google-chrome \
+fastfetch \
+gamemode \
+mangohud
+
+echo Done
